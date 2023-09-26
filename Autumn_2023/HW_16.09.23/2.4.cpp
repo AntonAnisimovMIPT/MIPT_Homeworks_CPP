29203f252624fa46853aca7c2bd9f34ed8c6d6a2
AntonAnisimovMIPT <127052051+AntonAnisimovMIPT@users.noreply.github.com>
2023-09-27 00:45:22 +0300

    fixed 2.4.cpp
    according to all the comments in the Vkontakte correspondence

--- Autumn_2023/HW_16.09.23/2.4.cpp
+++ Autumn_2023/HW_16.09.23/2.4.cpp
@@ -1,78 +1,87 @@
 #include <iostream>
 
 
-void Merge(int* inp_arr, int left, int middle, int right);
+void merge(int* a, int left, int middle, int right);
 
-// Сортировка подмассива с индексами в [left, right)
-void SubMergeSort(int* arr, int left, int right){
+void mergeSort(int* arr, int left, int right){
     if (left + 1 >= right) {
         return;
     }
 
-    auto middle =  (right + left) / 2;
-    SubMergeSort(arr, left, middle);
-    SubMergeSort(arr, middle, right);
-    Merge(arr, left, middle, right);
+    auto middle =  left + (right - left) / 2;
+    mergeSort(arr, left, middle);
+    mergeSort(arr, middle, right);
+    merge(arr, left, middle, right);
 }
 
-// Решим подзадачу слияния двух массивов
-void Merge(int* inp_arr, int left, int middle, int right){
-    auto first_iter = 0;
-    auto second_iter = 0;
-    auto res_arr = new int[right - left]{};
-
-    while (left + first_iter < middle && middle + second_iter < right) {
-        if (inp_arr[left + first_iter] < inp_arr[middle + second_iter]) {
-            res_arr[first_iter + second_iter] = inp_arr[left + first_iter];
-            first_iter += 1;
+void merge(int* a, int left, int middle, int right) {
+    
+    const auto size = right - left;
+    
+    auto tmp = new int[size]{};
+    
+    const auto begin = left;
+    const auto end = middle;
+    
+    for (auto i = 0; i < size; ++i) {
+        
+        if (left < end && middle < right && a[left] <= a[middle]) {
+            
+            tmp[i] = a[left++];
+            
         }
+        
+        else if (left < end && middle < right) {
+            
+            tmp[i] = a[middle++];
+            
+        }
+        
+        else if (left < end) {
+            
+            tmp[i] = a[left++];
+            
+        }
+        
         else {
-            res_arr[first_iter + second_iter] = inp_arr[middle + second_iter];
-            second_iter += 1;
+            
+            tmp[i] = a[middle++];
+            
         }
+        
     }
-
-    while (left + first_iter < middle) {
-        res_arr[first_iter + second_iter] = inp_arr[left + first_iter];
-        first_iter += 1;
+    
+    for (auto i = 0; i < size; ++i) {
+        
+        a[begin + i] = tmp[i];
+        
     }
-
-    while (middle + second_iter < right) {
-        res_arr[first_iter + second_iter] = inp_arr[middle + second_iter];
-        second_iter += 1;
-    }
-
-    for (auto i = 0; i < first_iter + second_iter; i++) {
-        inp_arr[left + i] = res_arr[i];
-    }
+    
+    delete[] tmp;
 }
 
-void InsertSort(int* arr, int left, int right) {
+
+void insertSort(int* arr, int left, int right) {
     auto size = right - left;
-    for (auto i = 1 ; i < size; ++i) {
-        auto pulled = arr[i];
-        auto j = i - 1;
-
-        while (j >= 0 && arr[j] > pulled) {
-            arr[j + 1] = arr[j];
-            j--;
+    for(auto i=1; i < size ; i++) {
+        for(auto j=i; j > 0 && arr[j-1] > arr[j]; j--) {
+            std::swap(arr[j-1], arr[j]);
         }
-        arr[j + 1] = pulled;
-    }
+    }     
 }
 
-void HybridSort(int* arr, int left, int right, int half_size) {
+void hybridSort(int* arr, int left, int right, int half_size) {
     if (right - left + 1 <= half_size) {
-        InsertSort(arr, left, right);
+        insertSort(arr, left, right);
     } else {
         if (left + 1 >= right) {
             return;
         }
 
-        auto middle =  (right + left) / 2;
-        SubMergeSort(arr, left, middle);
-        SubMergeSort(arr, middle, right);
-        Merge(arr, left, middle, right);
+        auto middle = left + (right - left) / 2;
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle, right);
+        merge(arr, left, middle, right);
     }
 }
 
@@ -93,7 +102,7 @@ int main() {
 
     }
 
-    HybridSort(need_to_sort, 0, size, half_size);
+    hybridSort(need_to_sort, 0, size, half_size);
 
     std::cout << "\nNew sorted array: ";
     for (auto i = 0; i < size; ++i) {
