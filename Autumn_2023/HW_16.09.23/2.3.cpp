9ed5a0fd0525bf50da0f7120768f8b1b50549883
AntonAnisimovMIPT <127052051+AntonAnisimovMIPT@users.noreply.github.com>
2023-09-27 00:28:23 +0300

    fixed 2.3.cpp
    fixed merge function in 2.3.cpp (according to all the comments in the Vkontakte correspondence).

--- Autumn_2023/HW_16.09.23/2.3.cpp
+++ Autumn_2023/HW_16.09.23/2.3.cpp
@@ -1,50 +1,63 @@
 #include <iostream>
-void Merge(int* inp_arr, int left, int middle, int right);
 
-// Сортировка подмассива с индексами в [left, right)
-void SubMergeSort(int* arr, int left, int right){
+void merge(int* a, int left, int middle, int right);
+
+void mergeSort(int* arr, int left, int right){
     if (left + 1 >= right) {
         return;
     }
 
-    auto middle =  (right + left) / 2;
-    SubMergeSort(arr, left, middle);
-    SubMergeSort(arr, middle, right);
-    Merge(arr, left, middle, right);
+    auto middle =  left + (right - left) / 2;
+    mergeSort(arr, left, middle);
+    mergeSort(arr, middle, right);
+    merge(arr, left, middle, right);
 }
 
-// Решим подзадачу слияния двух массивов
-void Merge(int* inp_arr, int left, int middle, int right){
-    auto first_iter = 0;
-    auto second_iter = 0;
-    auto res_arr = new int[right - left]{};
-
-    while (left + first_iter < middle && middle + second_iter < right) {
-        if (inp_arr[left + first_iter] < inp_arr[middle + second_iter]) {
-            res_arr[first_iter + second_iter] = inp_arr[left + first_iter];
-            first_iter += 1;
+void merge(int* a, int left, int middle, int right) {
+    
+    const auto size = right - left;
+    
+    auto tmp = new int[size]{};
+    
+    const auto begin = left;
+    const auto end = middle;
+    
+    for (auto i = 0; i < size; ++i) {
+        
+        if (left < end && middle < right && a[left] <= a[middle]) {
+            
+            tmp[i] = a[left++];
+            
         }
+        
+        else if (left < end && middle < right) {
+            
+            tmp[i] = a[middle++];
+            
+        }
+        
+        else if (left < end) {
+            
+            tmp[i] = a[left++];
+            
+        }
+        
         else {
-            res_arr[first_iter + second_iter] = inp_arr[middle + second_iter];
-            second_iter += 1;
+            
+            tmp[i] = a[middle++];
+            
         }
+        
     }
-
-    while (left + first_iter < middle) {
-        res_arr[first_iter + second_iter] = inp_arr[left + first_iter];
-        first_iter += 1;
+    
+    for (auto i = 0; i < size; ++i) {
+        
+        a[begin + i] = tmp[i];
+        
     }
-
-    while (middle + second_iter < right) {
-        res_arr[first_iter + second_iter] = inp_arr[middle + second_iter];
-        second_iter += 1;
-    }
-
-    for (auto i = 0; i < first_iter + second_iter; i++) {
-        inp_arr[left + i] = res_arr[i];
-    }
+    
+    delete[] tmp;
 }
-
 
 int main() {
     const auto size = 10;
@@ -62,7 +75,7 @@ int main() {
 
     }
 
-    SubMergeSort(need_to_sort, 0, size);
+    mergeSort(need_to_sort, 0, size);
 
     std::cout << "\nNew sorted array: ";
     for (auto i = 0; i < size; ++i) {
