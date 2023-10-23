e675397fcc7a446be5d46b54fa1df2e8754eea84
AntonAnisimovMIPT <127052051+AntonAnisimovMIPT@users.noreply.github.com>
2023-10-23 14:47:18 +0300

    Update 6.4.cpp

--- Autumn_2023/HW_14.10.23/6.4.cpp
+++ Autumn_2023/HW_14.10.23/6.4.cpp
@@ -1,15 +1,17 @@
+#include <iostream>
 
-
-template <typename Object, typename  Argument>
-auto to_call(Object& obj, Argument&& arg) {
-    return obj(arg);
+template <typename F, typename... Types>
+auto to_call(F&& f, Types&&... args) {
+    return f(std::forward <Types> (args)...);
 }
 
-
-template <typename Object, typename ... Arguments>
-auto to_call(Object& obj, Arguments&&... args) {
-    return obj(args...);
+double multiplication(double a, double b, double c) {
+    return a*b*c;
 }
 
+int main() {
 
-
+    
+    std::cout << to_call(multiplication, 1.0, 2.0, 3.0);
+    
+}
