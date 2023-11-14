44bb21d405dd77c09f1068dc43fa64ce1c9e928c
AntonAnisimovMIPT <127052051+AntonAnisimovMIPT@users.noreply.github.com>
2023-11-14 17:50:48 +0300

    Update 9.1.cpp

--- Autumn_2023/HW_11.11.23/9.1.cpp
+++ Autumn_2023/HW_11.11.23/9.1.cpp
@@ -1,1 +1,43 @@
-sdfgb
+#include <iostream>
+
+class Drawing_API_v1
+{
+public:
+
+    void draw() const { std::cout << "drawing API v1" << std::endl; }
+
+}; // class Drawing_API_v1 : public Drawing_API
+
+class Drawing_API_v2
+{
+public:
+
+    void draw() const { std::cout << "drawing API v2" << std::endl; }
+
+}; // class Drawing_API_v2 : public Drawing_API
+
+template <typename Drawing_API>
+class Handling
+{
+public:
+    explicit Handling(const Drawing_API & drawing_API) : m_drawing_API(drawing_API) {}
+
+    void draw() const {
+        m_drawing_API.draw();
+    }
+
+protected:
+
+    const Drawing_API & m_drawing_API;
+
+}; // class Shape
+
+
+
+int main()
+{
+
+    Handling<Drawing_API_v1>(Drawing_API_v1()).draw();
+    Handling<Drawing_API_v2>(Drawing_API_v2()).draw();
+    return 0;
+}
