97364b990eb7833aa685fb4aa2a47cbe5cf07041
AntonAnisimovMIPT <127052051+AntonAnisimovMIPT@users.noreply.github.com>
2023-11-28 22:23:15 +0300

    Update 11.3.cpp

--- Autumn_2023/HW_25.11.23/11.3.cpp
+++ Autumn_2023/HW_25.11.23/11.3.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Input epsilon: " << "\n";
-    auto input_epsilon = 0.0; // специально сделал тип double, а не float, чтобы точность была побольше
-    std::cin >> input_epsilon;
-    const auto epsilon = input_epsilon;
-
+constexpr auto e (double input) {
     auto x_n = 1.0; // это n-й член ряда
     auto sum = 1.0; // т.к по формуле Маклорена самый первый член ряда = 1
 
     int n = 0;
-    while (x_n >= epsilon) {
+    while (x_n >= input) {
         ++n;
         x_n /= n;
         sum += x_n;
     }
+    return sum;
 
-    std::cout << "e = " << sum;
 }
+
+int main() {
+    constexpr double precesion = 0.000001;
+    std::cout << e(precesion);
+    }
+
