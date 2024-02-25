f0fd4b27891b38fbaa6d5e2fc34ed18b0c23d1e4
AntonAnisimovMIPT <127052051+AntonAnisimovMIPT@users.noreply.github.com>
2024-02-25 21:46:33 +0300

    fixed Allocators_Timing + TESTS

--- Winter_Autmn_2024/2/Allocators_Timing/Allocators_Timing.cpp
+++ Winter_Autmn_2024/2/Allocators_Timing/Allocators_Timing.cpp
@@ -3,55 +3,7 @@
 #include <Stack_Allocator.h>
 #include <timer.h>
 
-template <typename Allocator>
-void Comparison(std::size_t size, std::size_t size_block, Timer& alloc_timer, Timer& dealloc_timer, int number_iterations) {
-    
-    try {
-        
-        for (int i = 0; i < number_iterations; ++i) {
-
-            if constexpr (!std::is_same_v<Allocator, Arena_Allocator> && !std::is_same_v<Allocator, Chain_Allocator>)
-            {
-                alloc_timer.start(); 
-                Allocator allocator(size);
-                auto ptr = allocator.allocate(size_block);
-                alloc_timer.pause(); 
-
-                dealloc_timer.start();
-                allocator.deallocate(ptr);
-                dealloc_timer.pause();
-
-            }
-            else if constexpr(std::is_same_v<Allocator, Chain_Allocator>)
-            {
-                alloc_timer.start(); 
-                Allocator allocator(size, size_block);
-                auto ptr = allocator.allocate();
-                alloc_timer.pause(); 
-
-                dealloc_timer.start();
-                allocator.deallocate(ptr);
-                dealloc_timer.pause();
-            }
-            
-            else {
-                {alloc_timer.start(); 
-                Allocator allocator(size);
-                auto ptr = allocator.allocate(size_block);
-                alloc_timer.pause(); 
-
-                dealloc_timer.start();}
-                dealloc_timer.pause();
-            }
-            
-            
-        }
-    std::cout << "Allocation time for Allocator: " << alloc_timer.get_mean_measure() << "\n";
-    std::cout << "Deallocation time for Allocator: " << dealloc_timer.get_mean_measure() << "\n";
-
-    alloc_timer.restart();
-    dealloc_timer.restart();
-
+auto Time_for_NewDelete(std::size_t size_block, Timer& alloc_timer, Timer& dealloc_timer, int number_iterations) {
     for (size_t i = 0; i < number_iterations; i++)
         {
             alloc_timer.start();
@@ -69,7 +21,32 @@ void Comparison(std::size_t size, std::size_t size_block, Timer& alloc_timer, Ti
 
     std::cout<< "Deallocation time for ::operator delete: " << dealloc_timer.get_mean_measure() << "\n"; 
     dealloc_timer.restart();
+}
 
+
+void Comparison_with_Arena(std::size_t size, std::size_t size_block, Timer& alloc_timer, Timer& dealloc_timer, int number_iterations) {
+    
+    try {
+        
+        for (int i = 0; i < number_iterations; ++i) {
+
+            alloc_timer.start(); 
+            Arena_Allocator allocator(size);
+            auto ptr = allocator.allocate(size_block);
+            alloc_timer.pause(); 
+
+            dealloc_timer.start();
+            dealloc_timer.pause();
+            
+        }
+    std::cout << "Allocation time for Arena: " << alloc_timer.get_mean_measure() << "\n";
+    std::cout << "Deallocation time for Arena: " << dealloc_timer.get_mean_measure() << "\n";
+
+    alloc_timer.restart();
+    dealloc_timer.restart();
+
+    Time_for_NewDelete(size_block, alloc_timer, dealloc_timer, number_iterations);
+    
     }
     catch(const std::exception& exception) {
         std::cout << exception.what() << "\n";
@@ -77,33 +54,108 @@ void Comparison(std::size_t size, std::size_t size_block, Timer& alloc_timer, Ti
     
 }
 
+void Comparison_with_Stack(std::size_t size, std::size_t size_block, Timer& alloc_timer, Timer& dealloc_timer, int number_iterations) {
+    
+    try {
+        
+        for (int i = 0; i < number_iterations; ++i) {
+
+            alloc_timer.start(); 
+            Stack_Allocator allocator(size);
+            auto ptr = allocator.allocate(size_block);
+            alloc_timer.pause(); 
+
+            dealloc_timer.start();
+            allocator.deallocate(ptr);
+            dealloc_timer.pause();
+
+        }
+    std::cout << "Allocation time for Stack: " << alloc_timer.get_mean_measure() << "\n";
+    std::cout << "Deallocation time for Stack: " << dealloc_timer.get_mean_measure() << "\n";
+
+    alloc_timer.restart();
+    dealloc_timer.restart();
+
+    Time_for_NewDelete(size_block, alloc_timer, dealloc_timer, number_iterations);
+
+    }
+    catch(const std::exception& exception) {
+        std::cout << exception.what() << "\n";
+    }
+
+}
+
+void Comparison_with_Chain(std::size_t size, std::size_t size_block, Timer& alloc_timer, Timer& dealloc_timer, int number_iterations) {
+    
+    try {
+        
+        for (int i = 0; i < number_iterations; ++i) {
+
+            alloc_timer.start(); 
+            Chain_Allocator allocator(size, size_block);
+            auto ptr = allocator.allocate();
+            alloc_timer.pause(); 
+
+            dealloc_timer.start();
+            allocator.deallocate(ptr);
+            dealloc_timer.pause();
+            
+        }
+    std::cout << "Allocation time for Chain: " << alloc_timer.get_mean_measure() << "\n";
+    std::cout << "Deallocation time for Chain: " << dealloc_timer.get_mean_measure() << "\n";
+
+    alloc_timer.restart();
+    dealloc_timer.restart();
+
+    Time_for_NewDelete(size_block, alloc_timer, dealloc_timer, number_iterations);
+
+    }
+    catch(const std::exception& exception) {
+        std::cout << exception.what() << "\n";
+    }
+    
+}
+    
 int main() {
 
-    const std::size_t size = 1024;
-    const std::size_t size_block_1 = 128;
-    const std::size_t size_block_2 = 512;
+    const std::size_t KB = 1024;
+    const std::size_t MB = 1024 * KB;
+    const std::size_t GB = 1024 * MB;
     Timer alloc_timer;
     Timer dealloc_timer;
 
-    std::cout << "\nTests for size_block_1\n";
-    std::cout << "---Arena_Allocator---\n";
-    Comparison<Arena_Allocator>(size, size_block_1, alloc_timer, dealloc_timer, 10);
+    std::cout<< "--Size = 1 KB--";
+    std::cout << "\nTests for size_block = 128 bytes:\n";
+    Comparison_with_Arena(KB, 128, alloc_timer, dealloc_timer, 100);
+    Comparison_with_Stack(KB, 128, alloc_timer, dealloc_timer, 100);
+    Comparison_with_Chain(KB, 128, alloc_timer, dealloc_timer, 100);
 
-    std::cout << "---Stack_Allocator---\n";
-    Comparison<Stack_Allocator>(size, size_block_1, alloc_timer, dealloc_timer, 10);
+    std::cout << "\nTests for size_block = 512 bytes:\n";
+    Comparison_with_Arena(KB, 128, alloc_timer, dealloc_timer, 100);
+    Comparison_with_Stack(KB, 128, alloc_timer, dealloc_timer, 100);
+    Comparison_with_Chain(KB, 128, alloc_timer, dealloc_timer, 100);
 
-    std::cout << "---Chain_Allocator---\n";
-    Comparison<Chain_Allocator>(size, size_block_1, alloc_timer, dealloc_timer, 10);
+    std::cout<< "--Size = 1 MB--";
+    std::cout << "\nTests for size_block = 128 KB:\n";
+    Comparison_with_Arena(MB, 128 * KB, alloc_timer, dealloc_timer, 100);
+    Comparison_with_Stack(MB, 128 * KB, alloc_timer, dealloc_timer, 100);
+    Comparison_with_Chain(MB, 128 * KB, alloc_timer, dealloc_timer, 100);
 
-    std::cout << "\nTests for size_block_2\n";
-    std::cout << "---Arena_Allocator---\n";
-    Comparison<Arena_Allocator>(size, size_block_2, alloc_timer, dealloc_timer, 10);
+    std::cout << "\nTests for size_block = 512 KB:\n";
+    Comparison_with_Arena(MB, 512 * KB, alloc_timer, dealloc_timer, 100);
+    Comparison_with_Stack(MB, 512 * KB, alloc_timer, dealloc_timer, 100);
+    Comparison_with_Chain(MB, 512 * KB, alloc_timer, dealloc_timer, 100);
 
-    std::cout << "---Stack_Allocator---\n";
-    Comparison<Stack_Allocator>(size, size_block_2, alloc_timer, dealloc_timer, 10);
+    std::cout<< "--Size = 1 GB--";
+    std::cout << "\nTests for size_block = 128 MB:\n";
+    Comparison_with_Arena(GB, 128 * MB, alloc_timer, dealloc_timer, 100);
+    Comparison_with_Stack(GB, 128 * MB, alloc_timer, dealloc_timer, 100);
+    Comparison_with_Chain(GB, 128 * MB, alloc_timer, dealloc_timer, 100);
 
-    std::cout << "---Chain_Allocator---\n";
-    Comparison<Chain_Allocator>(size, size_block_2, alloc_timer, dealloc_timer, 10);
+    std::cout << "\nTests for size_block = 512 MB:\n";
+    Comparison_with_Arena(GB, 512 * MB, alloc_timer, dealloc_timer, 100);
+    Comparison_with_Stack(GB, 512 * MB, alloc_timer, dealloc_timer, 100);
+    Comparison_with_Chain(GB, 512 * MB, alloc_timer, dealloc_timer, 100);
 
 
 }
