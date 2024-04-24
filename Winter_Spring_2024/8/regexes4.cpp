a42863a2f65f98c06c92e450b1459ae28ccca732
AntonAnisimovMIPT <anisimov-anton-main@mail.ru>
2024-04-24 12:42:06 +0300

    fixed regexes4

--- Winter_Spring_2024/8/regexes4.cpp
+++ Winter_Spring_2024/8/regexes4.cpp
@@ -3,12 +3,11 @@
 #include <string>
 
 auto formatted(const std::string& input_date) {
-
-    return std::regex_replace(input_date, std::regex(R"(\b(\d{2})/(\d{2})/(\d{4})\b)"), "$3.$2.$1");
+    return std::regex_replace(input_date, std::regex(R"(\b(\d{4})/(0[1-9]|1[0-2])/(0[1-9]|[1-2][0-9]|3[0-1])\b)"), "$1.$2.$3");
 }
 
 int main() {
-    std::string original_format = "01/04/2024";
+    std::string original_format = "2024/04/24";
 
     std::string formatted_text = formatted(original_format);
     std::cout << formatted_text << "\n";
